1784d1ec5bb63c43a6f2d3fc238b13cf3c3be9ce
Chamoda Pandithage <chamoda@xaventra.com>
2024-01-02 20:38:54 +0530

    Add consult and gptel

--- custom/compile-ts.el
+++ custom/compile-ts.el
@@ -2,5 +2,4 @@
 (defun treesitter-install-all-language-grammar ()
     (interactive)
     (mapc #'treesit-install-language-grammar (mapcar #'car treesit-language-source-alist))
-)
-
+    )
--- init.el
+++ init.el
@@ -69,15 +69,12 @@
   (project-switch-commands 'project-find-file))
 
 ;; AI
-(use-package c3po
-  :when (boundp 'OPENAI_API_KEY)
-  :vc (:fetcher github :repo "d1egoaz/c3po.el")
-  :init
-  (setq c3po-api-key OPENAI_API_KEY))
-
-(setq chatgpt-shell-openai-key OPENAI_API_KEY)
-(use-package chatgpt-shell
-  :vc (:fetcher github :repo "xenodium/chatgpt-shell"))
+(use-package gptel
+ :when (boundp 'OPENAI_API_KEY)
+ :config
+ (setq gptel-api-key OPENAI_API_KEY)
+ :bind
+ (("C-c RET" . gptel-send)))
 
 ;; Vertico
 (use-package vertico
@@ -102,6 +99,85 @@
               ("M-A" . marginalia-cycle))
   :init
   (marginalia-mode))
+
+;; Trying out consult
+(use-package consult
+  :bind (;; C-c bindings in `mode-specific-map'
+         ("C-c M-x" . consult-mode-command)
+         ("C-c h" . consult-history)
+         ("C-c k" . consult-kmacro)
+         ("C-c m" . consult-man)
+         ("C-c i" . consult-info)
+         ([remap Info-search] . consult-info)
+         ;; C-x bindings in `ctl-x-map'
+         ("C-x M-:" . consult-complex-command)     ;; orig. repeat-complex-command
+         ("C-x b" . consult-buffer)                ;; orig. switch-to-buffer
+         ("C-x 4 b" . consult-buffer-other-window) ;; orig. switch-to-buffer-other-window
+         ("C-x 5 b" . consult-buffer-other-frame)  ;; orig. switch-to-buffer-other-frame
+         ("C-x t b" . consult-buffer-other-tab)    ;; orig. switch-to-buffer-other-tab
+         ("C-x r b" . consult-bookmark)            ;; orig. bookmark-jump
+         ("C-x p b" . consult-project-buffer)      ;; orig. project-switch-to-buffer
+         ;; Custom M-# bindings for fast register access
+         ("M-#" . consult-register-load)
+         ("M-'" . consult-register-store)          ;; orig. abbrev-prefix-mark (unrelated)
+         ("C-M-#" . consult-register)
+         ;; Other custom bindings
+         ("M-y" . consult-yank-pop)                ;; orig. yank-pop
+         ;; M-g bindings in `goto-map'
+         ("M-g e" . consult-compile-error)
+         ("M-g f" . consult-flymake)               ;; Alternative: consult-flycheck
+         ("M-g g" . consult-goto-line)             ;; orig. goto-line
+         ("M-g M-g" . consult-goto-line)           ;; orig. goto-line
+         ("M-g o" . consult-outline)               ;; Alternative: consult-org-heading
+         ("M-g m" . consult-mark)
+         ("M-g k" . consult-global-mark)
+         ("M-g i" . consult-imenu)
+         ("M-g I" . consult-imenu-multi)
+         ;; M-s bindings in `search-map'
+         ("M-s d" . consult-find)                  ;; Alternative: consult-fd
+         ("M-s c" . consult-locate)
+         ("M-s g" . consult-grep)
+         ("M-s G" . consult-git-grep)
+         ("M-s r" . consult-ripgrep)
+         ("M-s l" . consult-line)
+         ("M-s L" . consult-line-multi)
+         ("M-s k" . consult-keep-lines)
+         ("M-s u" . consult-focus-lines)
+         ;; Isearch integration
+         ("M-s e" . consult-isearch-history)
+         :map isearch-mode-map
+         ("M-e" . consult-isearch-history)         ;; orig. isearch-edit-string
+         ("M-s e" . consult-isearch-history)       ;; orig. isearch-edit-string
+         ("M-s l" . consult-line)                  ;; needed by consult-line to detect isearch
+         ("M-s L" . consult-line-multi)            ;; needed by consult-line to detect isearch
+         ;; Minibuffer history
+         :map minibuffer-local-map
+         ("M-s" . consult-history)                 ;; orig. next-matching-history-element
+         ("M-r" . consult-history))                ;; orig. previous-matching-history-element
+
+  :hook (completion-list-mode . consult-preview-at-point-mode)
+  :init
+  (setq register-preview-delay 0.5
+        register-preview-function #'consult-register-format)
+
+  (advice-add #'register-preview :override #'consult-register-window)
+
+  (setq xref-show-xrefs-function #'consult-xref
+        xref-show-definitions-function #'consult-xref)
+
+  :config
+  (consult-customize
+   consult-theme :preview-key '(:debounce 0.2 any)
+   consult-ripgrep consult-git-grep consult-grep
+   consult-bookmark consult-recent-file consult-xref
+   consult--source-bookmark consult--source-file-register
+   consult--source-recent-file consult--source-project-recent-file
+   :preview-key '(:debounce 0.4 any))
+  )
+
+;; Version control
+;; Magit
+(use-package magit)
 
 ;; Using LSP
 (use-package eglot
@@ -213,4 +289,3 @@
 
 ;; Load custom files
 (load-file(locate-user-emacs-file "custom/compile-ts.el"))
-
