d1574098fee5629f4920aa150d04a603b9bc959d
Chamoda Pandithage <chamoda@xaventra.com>
2024-02-23 13:14:43 +0530

    New theme, performance improvement and more

--- init.el
+++ init.el
@@ -2,17 +2,17 @@
 (load "~/.emacs.d/.env.el" t)
 
 ;; Change default frame size
-(setq default-frame-alist '((width . 120) (height . 80)))
+(setq default-frame-alist '((width . 120) (height . 60)))
 
 ;; Fonts
-(set-face-attribute 'default nil :height 142)
+(set-face-attribute 'default nil :height 160)
 
 ;; No bell, no startup screen
 (setq ring-bell-function 'ignore
       inhibit-startup-screen t
       )
 
-;; Disable backup files (my be not a good idea?), lock files and auto save
+;; Disable backup files, lock files and auto save
 (setq make-backup-files nil
       create-lockfiles nil
       auto-save-default nil)
@@ -39,7 +39,9 @@
 (use-package isearch
   :ensure nil
   :init
-  (setq isearch-lazy-count t))
+  (setq isearch-lazy-count t)
+  (setq case-fold-search t)
+  )
 
 ;; Tramp for remote editing
 (use-package tramp
@@ -63,13 +65,11 @@
   :custom
   (project-switch-commands 'project-find-file))
 
-;; AI
-(use-package gptel
- :when (boundp 'OPENAI_API_KEY)
- :config
- (setq gptel-api-key OPENAI_API_KEY)
- :bind
- (("C-c RET" . gptel-send)))
+;; Offline documentation
+(use-package devdocs
+  :init
+  (setq shr-use-fonts nil))
+
 
 ;; Vertico
 (use-package vertico
@@ -95,41 +95,42 @@
   :init
   (marginalia-mode))
 
-; Trying out consult
+;; Show next key
+(use-package which-key
+  :init
+  (which-key-mode)
+  (setq which-key-idle-delay 0.8))
+
+
+;; Consult
 (use-package consult
-  :bind (;; C-c bindings in `mode-specific-map'
-         ("C-c M-x" . consult-mode-command)
+  :bind (("C-c M-x" . consult-mode-command)
          ("C-c h" . consult-history)
          ("C-c k" . consult-kmacro)
          ("C-c m" . consult-man)
          ("C-c i" . consult-info)
          ([remap Info-search] . consult-info)
-         ;; C-x bindings in `ctl-x-map'
-         ("C-x M-:" . consult-complex-command)     ;; orig. repeat-complex-command
-         ("C-x b" . consult-buffer)                ;; orig. switch-to-buffer
-         ("C-x 4 b" . consult-buffer-other-window) ;; orig. switch-to-buffer-other-window
-         ("C-x 5 b" . consult-buffer-other-frame)  ;; orig. switch-to-buffer-other-frame
-         ("C-x t b" . consult-buffer-other-tab)    ;; orig. switch-to-buffer-other-tab
-         ("C-x r b" . consult-bookmark)            ;; orig. bookmark-jump
-         ("C-x p b" . consult-project-buffer)      ;; orig. project-switch-to-buffer
-         ;; Custom M-# bindings for fast register access
+         ("C-x M-:" . consult-complex-command) 
+         ("C-x b" . consult-buffer)                
+         ("C-x 4 b" . consult-buffer-other-window) 
+         ("C-x 5 b" . consult-buffer-other-frame)  
+         ("C-x t b" . consult-buffer-other-tab)    
+         ("C-x r b" . consult-bookmark)            
+         ("C-x p b" . consult-project-buffer)      
          ("M-#" . consult-register-load)
-         ("M-'" . consult-register-store)          ;; orig. abbrev-prefix-mark (unrelated)
+         ("M-'" . consult-register-store)         
          ("C-M-#" . consult-register)
-         ;; Other custom bindings
-         ("M-y" . consult-yank-pop)                ;; orig. yank-pop
-         ;; M-g bindings in `goto-map'
+         ("M-y" . consult-yank-pop)                
          ("M-g e" . consult-compile-error)
-         ("M-g f" . consult-flymake)               ;; Alternative: consult-flycheck
-         ("M-g g" . consult-goto-line)             ;; orig. goto-line
-         ("M-g M-g" . consult-goto-line)           ;; orig. goto-line
-         ("M-g o" . consult-outline)               ;; Alternative: consult-org-heading
+         ("M-g f" . consult-flymake)               
+         ("M-g g" . consult-goto-line)             
+         ("M-g M-g" . consult-goto-line)           
+         ("M-g o" . consult-outline)               
          ("M-g m" . consult-mark)
          ("M-g k" . consult-global-mark)
          ("M-g i" . consult-imenu)
          ("M-g I" . consult-imenu-multi)
-         ;; M-s bindings in `search-map'
-         ("M-s d" . consult-find)                  ;; Alternative: consult-fd
+         ("M-s d" . consult-find)                  
          ("M-s c" . consult-locate)
          ("M-s g" . consult-grep)
          ("M-s G" . consult-git-grep)
@@ -138,17 +139,17 @@
          ("M-s L" . consult-line-multi)
          ("M-s k" . consult-keep-lines)
          ("M-s u" . consult-focus-lines)
-         ;; Isearch integration
          ("M-s e" . consult-isearch-history)
+	 
          :map isearch-mode-map
-         ("M-e" . consult-isearch-history)         ;; orig. isearch-edit-string
-         ("M-s e" . consult-isearch-history)       ;; orig. isearch-edit-string
-         ("M-s l" . consult-line)                  ;; needed by consult-line to detect isearch
-         ("M-s L" . consult-line-multi)            ;; needed by consult-line to detect isearch
-         ;; Minibuffer history
+         ("M-e" . consult-isearch-history)         
+         ("M-s e" . consult-isearch-history)       
+         ("M-s l" . consult-line)                  
+         ("M-s L" . consult-line-multi)            
+	 
          :map minibuffer-local-map
-         ("M-s" . consult-history)                 ;; orig. next-matching-history-element
-         ("M-r" . consult-history))                ;; orig. previous-matching-history-element
+         ("M-s" . consult-history)                 
+         ("M-r" . consult-history))                
 
   :hook (completion-list-mode . consult-preview-at-point-mode)
   :init
@@ -170,24 +171,48 @@
    :preview-key '(:debounce 0.4 any))
   )
 
-;; Version control
-;; Magit
-(use-package magit)
+;; Editor config
+(use-package editorconfig
+  :ensure t
+  :config
+  (editorconfig-mode 1))
 
 ;; Using LSP
 (use-package eglot
+  :init
+  (setq eglot-autoshutdown t)
+  (setq eglot-confirm-server-initiated-edits nil)
   :hook
   ((python-ts-mode js-ts-mode typescript-ts-mode tsx-ts-mode) . eglot-ensure))
 
-;; Auto Completion
-(global-set-key [remap dabbrev-expand] 'hippie-expand)
+;; LSP performance improvements
+(setq eldoc-idle-delay 0.5)
+(setq flymake-no-changes-timeout 2)
+(setq eldoc-echo-area-prefer-doc-buffer t)
+(fset #'jsonrpc--log-event #'ignore)
+(setq eglot-events-buffer-size 0)
+(setq eglot-sync-connect nil)
+(setq eglot-connect-timeout nil)
+(setq eglot-send-changes-idle-time 0.75)
 
+;; Garbage collector
+(use-package gcmh
+  :init
+  (setq gcmh-high-cons-threshold 536870912) ;; 512MB
+  (setq gcmh-verbose t)
+  (gcmh-mode 1))
+
+;; Auto Completion
+(global-set-key [remap dabbrev-expand] 'dabbrev-expand)
 (use-package corfu
   :custom  
-  (corfu-auto t) 
-  (corfu-auto-delay 1)
+  (corfu-auto t)
+  (corfu-auto-delay 0.10)
+  (corfu-auto-prefix 1)
   :init
-  (global-corfu-mode))
+  (global-corfu-mode)
+  (corfu-history-mode)
+  (corfu-popupinfo-mode))
 
 ;; Need corfu-terminal to get auto-complete working in terminal
 (use-package corfu-terminal
@@ -208,9 +233,12 @@
   (setq org-default-notes-file '("~/org/tasks.org"))
   (setq org-log-into-drawer t)
   (setq org-log-done 'time)
+  (setq org-cycle-separator-lines 0)
+  (setq org-startup-folded t)
   (setq org-return-follows-link  t)
   (setq org-hide-emphasis-markers t)
-
+  (setq org-use-speed-commands t)
+  
   (setq org-habit-show-habits-only-for-today nil)
   
   (setq org-agenda-repeating-timestamp-show-all nil)
@@ -220,27 +248,54 @@
           (todo priority-down category-keep)
           (tags priority-down category-keep)
           (search category-keep)))
+  (print org-format-latex-options)
+  (setq org-format-latex-options (plist-put org-format-latex-options :scale 3.0))
+  (set-default 'preview-scale-function 1.2)
+  (setq org-startup-with-inline-images t)
+  (setq org-image-actual-width nil)
+  (setq org-confirm-babel-evaluate nil)
+  
   :hook 
   ((org-mode . org-indent-mode)
-   (org-mode . visual-line-mode))
+   (org-mode . visual-line-mode)
+   (org-mode . turn-on-flyspell))
   :bind
   (
    ("C-c l" . org-store-link)
    ("C-c a" . org-agenda)
    ("C-c c" . org-capture)
    )
-  :custom
-  (use-package org-contrib))
+  :config
+  (use-package org-contrib) ;; this is not running?
+  (use-package ob-mermaid)
+  (require 'org-checklist)
+  (require 'org-tempo)
 
+  (setq org-babel-python-command "python3")
+  
+  (org-babel-do-load-languages
+   'org-babel-load-languages
+   '((python . t)(shell . t)(mermaid . t)))
+  (add-to-list 'org-modules 'org-tempo t)
+  (add-to-list 'org-modules 'org-checklist t)
+
+  (add-hook 'org-babel-after-execute-hook 'org-redisplay-inline-images))
+
+
+;; Org roam
 (use-package emacsql-sqlite-builtin)
-
 (use-package org-roam
   :init
   (setq org-roam-v2-ack t)
   :custom
-  (org-roam-directory "~/org-roam")
-  (org-roam-complete-everywhere)
-  (org-roam-database-connector 'emacsql-sqlite))
+  (org-roam-directory "~/org/roam")
+  (org-roam-complete-everywhere t)
+  (org-roam-db-autosync-mode t)
+  (org-roam-database-connector 'sqlite-builtin)
+  :bind (("C-c n l" . org-roam-buffer-toggle)
+	 ("C-c n f" . org-roam-node-find)
+	 ("C-c n i" . org-roam-node-insert)
+	 :map org-mode-map ("C-M-i" . completion-at-point)))
 
 ;; Install lang support
 (add-to-list 'safe-local-variable-values '(indent-tabs-mode . nil))
@@ -287,29 +342,27 @@
 (add-to-list 'auto-mode-alist '("\\.[tj]sx?\\'" . tsx-ts-mode))
 (add-to-list 'auto-mode-alist '("\\.mdx\\'" . markdown-mode))
 
-;; Load theme
-(load-theme 'modus-vivendi t)
+;; Spelling and grammar
+(use-package languagetool
+  :init
+  (setq languagetool-java-arguments '("-Dfile.encoding=UTF-8")
+	languagetool-suggestion-level "PICKY"
+	languagetool-mother-tongue "en-US"
+	languagetool-correction-language "en-US"
+	languagetool-console-command "~/.languagetool/languagetool-commandline.jar"
+	languagetool-server-command "~/.languagetool/languagetool-server.jar")
+  )
 
-;; Custom background color for modus-vivendi
-(custom-set-faces
- ;; custom-set-faces was added by Custom.
- ;; If you edit it by hand, you could mess it up, so be careful.
- ;; Your init file should contain only one such instance.
- ;; If there is more than one, they won't work right.
- '(default ((t (:background "#18181b")))))
+;; Theme
+(use-package gruber-darker-theme)
+(load-theme 'gruber-darker t)
+(add-to-list 'default-frame-alist `(font . ,"Iosevka"))
 
 ;; User Interface
-;; Get rid of scroll bar, menubar and toolbar
 (menu-bar-mode -1)
 (tool-bar-mode -1)
 (scroll-bar-mode -1)
 
 ;; Load custom files
 (load-file(locate-user-emacs-file "custom/compile-ts.el"))
-(custom-set-variables
- ;; custom-set-variables was added by Custom.
- ;; If you edit it by hand, you could mess it up, so be careful.
- ;; Your init file should contain only one such instance.
- ;; If there is more than one, they won't work right.
- '(package-selected-packages
-   '(emacsql-sqlite-builtin vertico vc-use-package pet org-roam orderless markdown-mode marginalia magit gptel corfu-terminal consult)))
+
